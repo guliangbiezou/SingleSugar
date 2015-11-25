e6051b9129a28a377d1e636ec562f88ae29e35fc
guliangbiezou <we33love@sina.com>
2015-11-25 16:20:45 +0800

    分类和我  主页面的完成 25/11/2015  4:20

--- app/src/main/java/la/tietie/singlesugar/fragments/ProfileFragment.java
+++ app/src/main/java/la/tietie/singlesugar/fragments/ProfileFragment.java
@@ -1,22 +1,79 @@
 package la.tietie.singlesugar.fragments;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import la.tietie.singlesugar.R;
+import la.tietie.singlesugar.adapters.ProfilePagerAdapter;
+import la.tietie.singlesugar.view.CustomViewPager;
+import la.tietie.singlesugar.view.ScrollView_CanStop;
 
 /**
  * Created by steven on 2015/11/22.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment  {
+
+    private ScrollView_CanStop sv;
+    private LinearLayout center1;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.profile_fragment,null);
+        View view = inflater.inflate(R.layout.profile_fragment, null);
+        initView(view);
         return view;
     }
+
+    private void initView(View view) {
+        sv = (ScrollView_CanStop) view.findViewById(R.id.sv_profile);
+        View head = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+        sv.addHead(head);
+        center1 = (LinearLayout) view.findViewById(R.id.ll_scroll_center1);
+        sv.setCenter1(center1);
+//        View foot1 = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+//        View foot2 = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+//        View foot3 = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+//        View foot4 = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+//        View foot5 = LayoutInflater.from(getContext()).inflate(R.layout.profile_scroll_head,null);
+//        sv.addFoot(foot1);
+//        sv.addFoot(foot2);
+//        sv.addFoot(foot3);
+//        sv.addFoot(foot4);
+//        sv.addFoot(foot5);
+        View view1 = LayoutInflater.from(getContext()).inflate(R.layout.layout_scroll_foot_hei, null);
+        View view2 = LayoutInflater.from(getContext()).inflate(R.layout.layout_scroll_foot_hei,null);
+        List<View> datas = new ArrayList<>();
+        datas.add(view1);
+        datas.add(view2);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        view1.setLayoutParams(params);
+        view2.setLayoutParams(params);
+        CustomViewPager pager = new CustomViewPager(getContext());
+        ProfilePagerAdapter adapter = new ProfilePagerAdapter();
+        pager.setAdapter(adapter);
+        params.height = 800;
+        pager.setLayoutParams(params);
+        adapter.setDatas(datas);
+        sv.addFoot(pager);
+    }
+
+
+
 }
--- app/src/main/java/la/tietie/singlesugar/utils/JsonUtil.java
+++ app/src/main/java/la/tietie/singlesugar/utils/JsonUtil.java
@@ -1,5 +1,7 @@
 package la.tietie.singlesugar.utils;
 
+import android.util.Log;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -10,6 +12,8 @@ import java.util.List;
 
 import la.tietie.singlesugar.bean.DanPingData;
 import la.tietie.singlesugar.bean.DanPingItem;
+import la.tietie.singlesugar.bean.FenLeiZhuanTi;
+import la.tietie.singlesugar.bean.FenleiChannels;
 
 /**
  * Created by Administrator on 2015/11/23 0023.
@@ -17,11 +21,30 @@ import la.tietie.singlesugar.bean.DanPingItem;
 public class JsonUtil {
 
     public static List<DanPingItem> JsonDanping(String jsonStr) {
-        List<DanPingItem> datas = new ArrayList<>();
+        List<DanPingItem> datas;
         JSONObject all = JSONObject.parseObject(jsonStr);
         JSONObject data = all.getJSONObject("data");
         String result = data.getString("items");
         datas = JSON.parseArray(result,DanPingItem.class);
+        return  datas;
+    }
+
+    public static List<FenLeiZhuanTi> JsonFenLeiZhuanTi (String jsonStr) {
+        List<FenLeiZhuanTi> datas;
+        JSONObject object = JSONObject.parseObject(jsonStr);
+        object = object.getJSONObject("data");
+        String result = object.getString("collections");
+        datas = JSON.parseArray(result,FenLeiZhuanTi.class);
+        Log.e("print",datas.toString());
+        return  datas;
+    }
+
+    public static List<FenleiChannels>  JsonChannels(String jsonStr) {
+        List<FenleiChannels> datas;
+        JSONObject object = JSONObject.parseObject(jsonStr);
+        object = object.getJSONObject("data");
+        String result = object.getString("channel_groups");
+        datas = JSON.parseArray(result,FenleiChannels.class);
         return  datas;
     }
 }
